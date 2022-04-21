b6240c487753f12cbd5a67113dde23747dd1af75
lefterisYA <elefterios.lundell@yaelev.se>
2022-04-21 15:32:35 +0200

    Projekt skapad med spring initializr

---
+++ src/test/java/com/sogetirockstars/sogetipaintinglotteryserver/SogetiPaintingLotteryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sogetirockstars.sogetipaintinglotteryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SogetiPaintingLotteryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
