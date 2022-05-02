d8284ac9975b1cb48b92ee43745895dd73a4b7e3
lefterisYA <elefterios.lundell@yaelev.se>
2022-05-02 15:30:32 +0200

    fix till review https://github.com/sogeti-rockstars/sogeti-painting-lottery-server/pull/4#discussion_r862842412

--- src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/controller/LotteryItemController.java
+++ src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/controller/LotteryItemController.java
@@ -1,23 +1,31 @@
 package com.sogetirockstars.sogetipaintinglotteryserver.controller;
 
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import com.sogetirockstars.sogetipaintinglotteryserver.exception.IdException;
 import com.sogetirockstars.sogetipaintinglotteryserver.model.LotteryItem;
 import com.sogetirockstars.sogetipaintinglotteryserver.service.LotteryItemService;
 import com.sogetirockstars.sogetipaintinglotteryserver.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.List;
 
 /**
  * PaintingController:
@@ -115,9 +123,8 @@ public class LotteryItemController {
     public ResponseEntity<?> update(@RequestBody LotteryItem item) {
         try {
             return new ResponseEntity<>(lotteryItemService.update(item), HttpStatus.OK);
-        } catch (Exception e) {
-            String msg = "Item with value "+ item.getId()+" not found.";
-            return new ResponseEntity<>(msg, HttpStatus.NOT_FOUND);
+        } catch (IdException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
 }
