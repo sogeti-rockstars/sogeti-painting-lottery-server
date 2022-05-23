f96e2c1915f2e4011189343e80fca6a59c22c025
jessica pamlin <jessicapamlins@hotmail.se>
2022-05-23 10:04:30 +0200

    metod för att hämta id namn o datum

--- src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/controller/LotteryController.java
+++ src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/controller/LotteryController.java
@@ -27,9 +27,15 @@ public class LotteryController {
     /**
      * Returns all lottery
      */
+
     @GetMapping
     public List<Lottery> getAll() {
         return lotteryService.getAll();
+    }
+
+    @GetMapping(value = "allSmall")
+    public List<LotteryService.LotterySmall> getAllSmall() {
+        return lotteryService.getAllSmall();
     }
 
     /**
@@ -45,6 +51,8 @@ public class LotteryController {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
     }
+
+
 
     @GetMapping(value = "spin-with-item/{id}")
     public ResponseEntity<?> spinTheWheelRandomItem(@PathVariable Long id) {
