57eef33b90e9cbd1f0d085328e27f8ba7790e86c
ottomann1 <otto.kostmann@hotmail.com>
2022-06-07 11:01:27 +0200

    fixat relationen mellan lottery och lotteryitem

--- src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/controller/LotteryController.java
+++ src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/controller/LotteryController.java
@@ -1,9 +1,5 @@
 package com.sogetirockstars.sogetipaintinglotteryserver.controller;
 
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
-
 import com.sogetirockstars.sogetipaintinglotteryserver.exception.AllContestantsTakenException;
 import com.sogetirockstars.sogetipaintinglotteryserver.exception.EmptyLotteryWinnerAssignmentException;
 import com.sogetirockstars.sogetipaintinglotteryserver.exception.IdException;
@@ -12,21 +8,17 @@ import com.sogetirockstars.sogetipaintinglotteryserver.model.Lottery;
 import com.sogetirockstars.sogetipaintinglotteryserver.model.LotteryItem;
 import com.sogetirockstars.sogetipaintinglotteryserver.model.Winner;
 import com.sogetirockstars.sogetipaintinglotteryserver.service.LotteryService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController @RequestMapping("api/v1/lottery")
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/lottery")
 public class LotteryController {
     private final LotteryService lotteryService;
 
@@ -140,10 +132,20 @@ public class LotteryController {
         }
     }
 
-    @PutMapping(value = "{id}/addItem")
-    public ResponseEntity<?> addNewItemToLottery(@PathVariable Long id, @RequestBody LotteryItem lotteryItem) {
+    @PutMapping(value = "addItem/{id}")
+    public ResponseEntity<?> addItemToLottery(@PathVariable Long id, @RequestBody LotteryItem lotteryItem) {
         try {
-            return new ResponseEntity<>(lotteryService.addNewItemToLottery(id, lotteryItem), HttpStatus.OK);
+            return new ResponseEntity<>(lotteryService.addItemToLottery(id, lotteryItem), HttpStatus.OK);
+        } catch (IdException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping(value = "editItem/{id}")
+    public ResponseEntity<?> editItemToLottery(@PathVariable Long id, @RequestBody LotteryItem lotteryItem) {
+        System.out.println(id + "++-" + lotteryItem.toString());
+        try {
+            return new ResponseEntity<>(lotteryService.editItemToLottery(id, lotteryItem), HttpStatus.OK);
         } catch (IdException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
         }
