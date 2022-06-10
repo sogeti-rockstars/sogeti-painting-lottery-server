08b4465f0bbe59c40ce70abe023a047e72566bf4
lefterisYA <elefterios.lundell@yaelev.se>
2022-06-10 13:30:27 +0200

    -

--- src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/service/PhotoService.java
+++ src/main/java/com/sogetirockstars/sogetipaintinglotteryserver/service/PhotoService.java
@@ -1,11 +1,9 @@
 package com.sogetirockstars.sogetipaintinglotteryserver.service;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
